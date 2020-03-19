f6d709256d9b86f96ca25d5fa5795bf18f914582
egmacaldo <egmacaldo@up.edu.ph>
2020-03-19 17:38:04 +0800

    fix: two-way binding by created string resource values

--- app/src/main/java/com/example/macaldo_exer5_lightsoutv2/Values.kt
+++ app/src/main/java/com/example/macaldo_exer5_lightsoutv2/Values.kt
@@ -1,8 +1,5 @@
 package com.example.macaldo_exer5_lightsoutv2
 
-class Values {
-    val title: String = "Lights Out!"
-    val nameHint: String = "Enter Name"
-    val play: String = "Play!"
+class Values (
     var name: String = ""
-}
+)
