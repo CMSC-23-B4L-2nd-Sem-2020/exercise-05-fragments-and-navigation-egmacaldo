af417f5f3d4ae91963756fe539b724915a2035d6
egmacaldo <egmacaldo@up.edu.ph>
2020-03-19 16:37:08 +0800

    feat: created text views and button in title fragment

--- app/src/main/java/com/example/macaldo_exer5_lightsoutv2/TitleFragment.kt
+++ app/src/main/java/com/example/macaldo_exer5_lightsoutv2/TitleFragment.kt
@@ -14,12 +14,17 @@ import com.example.macaldo_exer5_lightsoutv2.databinding.FragmentTitleBinding
  */
 class TitleFragment : Fragment() {
 
+    // reference the data class
+    private val values: Values = Values()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
             inflater,
             R.layout.fragment_title, container, false
         )
+        binding.values = values
+
 
         return binding.root
     }
--- app/src/main/java/com/example/macaldo_exer5_lightsoutv2/Values.kt
+++ app/src/main/java/com/example/macaldo_exer5_lightsoutv2/Values.kt
@@ -2,4 +2,7 @@ package com.example.macaldo_exer5_lightsoutv2
 
 class Values {
     val title: String = "Lights Out!"
+    val nameHint: String = "Enter Name"
+    val play: String = "Play!"
+    var name: String = ""
 }
